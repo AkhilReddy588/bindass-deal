c41c1f7816ac7e0843e1da151da207367c30e145
Nerati Akhil Reddy <akhil990836@gmail.com>
2024-06-11 12:44:32 +0530

    bindass

--- src/components/ProtectedRoute/index.js
+++ src/components/ProtectedRoute/index.js
@@ -1,8 +1,8 @@
 import {Redirect, Route} from 'react-router-dom'
-import Cookie from 'js-cookie'
+import Cookies from 'js-cookie'
 
 const ProtectedRoute = props => {
-  const token = Cookie.get('token')
+  const token = Cookies.get('token')
   if (token === undefined) {
     return <Redirect to="/login" />
   }
